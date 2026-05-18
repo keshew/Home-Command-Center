b50cd37399b2ef851d34a65f6ccb25778d7fc915
keshew <artyom.korotckov2017@yandex.ru>
2026-05-18 21:09:46 +0500

    Initial Commit

---
+++ Home Command Center/Home_Command_CenterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Home_Command_CenterApp.swift
+//  Home Command Center
+//
+//  Created by Артём Коротков on 18.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct Home_Command_CenterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
